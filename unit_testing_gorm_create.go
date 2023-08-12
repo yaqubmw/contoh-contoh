4399e69c28346d7ef97319114104240c3110c0d8
Yaqub Muflih Wiraatmaja <84486962+yaqubmw@users.noreply.github.com>
2023-08-12 19:35:07 +0700

    Update unit_testing_gorm_create.go

--- unit_testing_gorm_create.go
+++ unit_testing_gorm_create.go
@@ -31,11 +31,19 @@ func (suite *StatusLeaveRepositorySuite) SetupTest() {
 	suite.repo = NewStatusLeaveRepository(gormDB)
 }
 
-func (suite *StatusLeaveRepositorySuite) TestCreate_Success() {
-	payload := model.StatusLeave{
-		ID:              "1",
-		StatusLeaveName: "Pending",
+var dataDummy = []model.StatusLeave{
+		{
+			ID:              "1",
+			StatusLeaveName: "Pending",
+		},
+		{
+			ID:              "2",
+			StatusLeaveName: "Approved",
+		},
 	}
+
+func (suite *StatusLeaveRepositorySuite) TestCreate() {
+	payload := dataDummy[0]
 
 	suite.mocksql.ExpectBegin()
 	suite.mocksql.ExpectExec("INSERT INTO \"status_leave\" (.+)").WithArgs(payload.ID, payload.StatusLeaveName).WillReturnResult(sqlmock.NewResult(1, 1))
@@ -45,12 +53,10 @@ func (suite *StatusLeaveRepositorySuite) TestCreate_Success() {
 	assert.NoError(suite.T(), err)
 }
 
-func (suite *StatusLeaveRepositorySuite) TestGet_Success() {
+func (suite *StatusLeaveRepositorySuite) TestGet() {
 	statusLeaveID := "1"
-	expectedStatusLeave := model.StatusLeave{
-		ID:              statusLeaveID,
-		StatusLeaveName: "Pending",
-	}
+	expectedStatusLeave := dataDummy[0]
+
 	rows := sqlmock.NewRows([]string{"id", "status_leave_name"})
 	rows.AddRow(expectedStatusLeave.ID, expectedStatusLeave.StatusLeaveName)
 	expectedQuery := `SELECT \* FROM "status_leave" WHERE id = \$1`
@@ -60,10 +66,26 @@ func (suite *StatusLeaveRepositorySuite) TestGet_Success() {
 	assert.NoError(suite.T(), err)
 	assert.Equal(suite.T(), expectedStatusLeave, result)
 
-	// Ensure all expectations were met
 	assert.NoError(suite.T(), suite.mocksql.ExpectationsWereMet())
 }
 
+func (suite *StatusLeaveRepositorySuite) TestList() {
+	expectedStatusLeaves := dataDummy
+
+	rows := sqlmock.NewRows([]string{"id", "status_leave_name"})
+	for _, statusLeave := range expectedStatusLeaves {
+		rows.AddRow(statusLeave.ID, statusLeave.StatusLeaveName)
+	}
+
+	expectedQuery := `SELECT \* FROM "status_leave"`
+	suite.mocksql.ExpectQuery(expectedQuery).WillReturnRows(rows)
+
+	result, err := suite.repo.List()
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), expectedStatusLeaves, result)
+
+	assert.NoError(suite.T(), suite.mocksql.ExpectationsWereMet())
+}
 
 func TestStatusLeaveRepositorySuite(t *testing.T) {
 	suite.Run(t, new(StatusLeaveRepositorySuite))
